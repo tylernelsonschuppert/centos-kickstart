f779cb8b6fe1c2e9d54ab662e33124e3813c0987
Tyler Nelson-Schuppert <26035749+tylernelsonschuppert@users.noreply.github.com>
2020-06-10 14:21:04 -0400

    Updating missing gateway input in stage1.sh

--- stage1.sh
+++ stage1.sh
@@ -5,6 +5,8 @@ echo "Please input static IP to set for this server (ex. 192.168.5.2): "
 read IPADDR
 echo "Please input CIDR prefix to set for this server (ex. 24): "
 read PREFIX
+echo "Please input gateway address to set for this server (ex. 192.168.5.1): "
+read GATEWAY
 echo "Please input DNS server to set for this server (ex. 192.168.5.1): "
 read DNS1
 
