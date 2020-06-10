cffd00fc496ac550206971dfa892e34eb2f24522
Tyler Nelson-Schuppert <26035749+tylernelsonschuppert@users.noreply.github.com>
2020-06-10 14:04:24 -0400

    Really updating hostname variable in stage1.sh

--- stage1.sh
+++ stage1.sh
@@ -1,5 +1,5 @@
 echo "Please input hostname to set for this server (ex. centos-1.local.tylernelsonschuppert.com): "
-read $HOSTNAME
+read $HOST
 echo "Please input static IP to set for this server (ex. 192.168.5.2): "
 read $IPADDR
 echo "Please input CIDR prefix to set for this server (ex. 24): "
@@ -8,7 +8,7 @@ echo "Please input DNS server to set for this server (ex. 192.168.5.1): "
 read $DNS1
 
 dnf update -y
-echo "$HOSTNAME" > /etc/hostname
+echo "$HOST" > /etc/hostname
 cat /etc/sysconfig/network-scripts/ifcfg-ens192 | sed 's/dhcp/none/g' > /etc/sysconfig/network-scripts/ifcfg-ens192
 echo -e "IPADDR=$HOSTNAME\nPREFIX=$PREFIX\nGATEWAY=$GATEWAY\nDNS1=$DNS1\n" >> /etc/sysconfig/network-scripts/ifcfg-ens192 
 systemctl stop firewalld
